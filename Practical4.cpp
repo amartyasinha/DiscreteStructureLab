d86f91b57f4d66300e45f24668fb936d02e0766c
amartya <amartyasinha918@gmail.com>
2021-07-28 10:53:16 +0530

    Commiting Discrete Structures Files

--- Practical4.cpp
+++ Practical4.cpp
@@ -1,12 +1,16 @@
+//
+// Created by amart on 7/14/2021.
+//
+
 #include<iostream>
 using namespace std;
 
-class Relation{
+class RELATION{
     int n;
     int set1[10];
     int arr[10][10];
 public:
-    Relation(){
+    RELATION(){
         cout<<"Enter cardinality of set A: ";
         cin>>n;
         for(int i=0;i<n;i++){
@@ -14,106 +18,105 @@ public:
             cin>>set1[i];
         }
     }
-    void inputRelation();
-    void matrixDisplay();
-    bool chkReflexive();
-    bool chkSymmetric();
-    bool chkAntisymmetric();
-    bool chkTransitive();
-};
-void Relation::inputRelation(){
-    cout<<"Press 1 for yes or 0 for no.\n";
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<"Is there relation from element "<<set1[i]<<" to "<<set1[j]<<" :";
-            cin>>arr[i][j];
-            if(arr[i][j]>1){
-                abort();
-            }
-        }
-    }
-}
-void Relation::matrixDisplay(){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-}
-bool Relation::chkReflexive(){
-    bool flag;
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i][i]==1)
-        {
-            flag=true;
-        }
-        else
-        {
-            flag=false;
-            break;
-        }
-    }
-    return flag;
-}
-bool Relation::chkSymmetric(){
-    bool flag =true;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if((arr[i][j]!=arr[j][i])){
-                flag=false;
-                break;
-            }
-        }
-    }
-    return flag;
-}
-bool Relation::chkAntisymmetric(){
-    bool flag =true;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if((arr[i][j]==1)&&(arr[j][i]==1)&&(i!=j)){
-                flag=false;
-                break;
-            }
-        }
-    }
-    return flag;
-}
-bool Relation::chkTransitive(){
-    bool flag=true;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            for(int k=0;k<n;k++){
-                if(arr[i][k]==1 && arr[k][j]==1){
-                    if(arr[i][j]==0){
-                        flag=false;
-                        break;
-                    }
+    void inputRelation() {
+        cout<<"Press 1 for yes or 0 for no.\n";
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<"Is there relation from element "<<set1[i]<<" to "<<set1[j]<<" :";
+                cin>>arr[i][j];
+                if(arr[i][j]>1){
+                    abort();
                 }
             }
         }
     }
-    return flag;
-}
+
+    void matrixDisplay() {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    bool chkReflexive() {
+        bool flag;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i][i]==1)
+            {
+                flag=true;
+            }
+            else
+            {
+                flag=false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    bool chkSymmetric() {
+        bool flag =true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if((arr[i][j]!=arr[j][i])){
+                    flag=false;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+
+    bool chkAntisymmetric() {
+        bool flag =true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if((arr[i][j]==1)&&(arr[j][i]==1)&&(i!=j)){
+                    flag=false;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+    bool chkTransitive() {
+        bool flag=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    if(arr[i][k]==1 && arr[k][j]==1){
+                        if(arr[i][j]==0){
+                            flag=false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+};
+
 int main(){
-    Relation a;
+    RELATION a;
     a.inputRelation();
     cout<<"\nThe Matrix form of graph is: \n";
     a.matrixDisplay();
 
     if(a.chkReflexive() && a.chkSymmetric() && a.chkTransitive()){
-        cout<<"Relation is Equivalent\n";
+        cout<<"RELATION is Equivalent\n";
     }
     else{
-        cout<<"Relation is not Equivalent\n";
+        cout<<"RELATION is not Equivalent\n";
     }
     if(a.chkReflexive() && a.chkAntisymmetric() && a.chkTransitive()){
-        cout<<"Relation is Partial Ordered\n";
+        cout<<"RELATION is Partial Ordered\n";
     }
     else{
-        cout<<"Relation is not Partial Ordering\n";
+        cout<<"RELATION is not Partial Ordering\n";
     }
     return 0;
 }
